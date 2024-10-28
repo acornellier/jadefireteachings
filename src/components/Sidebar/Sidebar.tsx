d77fa4c2f5010ccd6f1d7e54826f8aab170968f9
Alex <alex@jintronix.com>
2024-10-29 08:55:59 +0900

    sidebar highlight active

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,24 +1,50 @@
-import { type Dispatch, type SetStateAction, useEffect, useState } from 'react'
+import { type Dispatch, type SetStateAction, useCallback, useEffect, useState } from 'react'
 import { SidebarLink, type SidebarLinkProps } from './SidebarLink.tsx'
 import { SidebarButton } from './SidebarButton.tsx'
+import { useWindowEvent } from '../../util/hooks/useWindowEvent.ts'
 
 interface SidebarProps {
   collapsed: boolean
   setCollapsed: Dispatch<SetStateAction<boolean>>
 }
 
+interface SidebarLinkElement extends SidebarLinkProps {
+  element: HTMLElement
+}
+
+function isHTMLElement(node: Node): node is HTMLElement {
+  return node instanceof HTMLElement
+}
+
 export function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
-  const [headers, setHeaders] = useState<SidebarLinkProps[]>([])
+  const [links, setLinks] = useState<SidebarLinkElement[]>([])
 
   const hiddenMedium = collapsed ? 'hidden' : ''
 
   useEffect(() => {
-    const found: SidebarLinkProps[] = []
-    for (const node of document.querySelectorAll('h2,h3')) {
-      found.push({ label: node.innerHTML, headingType: node.tagName.toLowerCase() as 'h2' | 'h3' })
-    }
-    setHeaders(found)
+    setLinks(
+      Array.from(document.querySelectorAll('h2,h3'))
+        .filter(isHTMLElement)
+        .map<SidebarLinkElement>((element) => ({
+          element: element,
+          active: false,
+          label: element.innerHTML,
+          headingType: element.tagName.toLowerCase() as 'h2' | 'h3',
+        })),
+    )
   }, [])
+
+  const handleScroll = useCallback(() => {
+    const activeLink = links.find(({ element }) => element.getBoundingClientRect().top > 0)
+    setLinks(
+      links.map((link) => ({
+        ...link,
+        active: link === activeLink,
+      })),
+    )
+  }, [links])
+
+  useWindowEvent('scroll', handleScroll)
 
   const onClickXs = async () => {
     setCollapsed(false)
@@ -41,9 +67,9 @@ export function Sidebar({ collapsed, setCollapsed }: SidebarProps) {
       <a id="toc" />
       <div className={`w-full sm:w-auto whitespace-nowrap pr-4 relative ${hiddenMedium} lg:block`}>
         <div className="flex flex-col items-center sm:items-start gap-2 sticky top-8 text-lg">
-          <SidebarLink label="Jadefire Teachings" headingType="h2" />
-          {headers.map(({ label, ...props }) => (
-            <SidebarLink key={label} label={label} {...props} />
+          <SidebarLink label="Jadefire Teachings" active={false} headingType="h2" />
+          {links.map(({ label, active, headingType }) => (
+            <SidebarLink key={label} active={active} label={label} headingType={headingType} />
           ))}
         </div>
       </div>
--- src/components/Sidebar/SidebarLink.tsx
+++ src/components/Sidebar/SidebarLink.tsx
@@ -2,21 +2,23 @@ import { useCallback } from 'react'
 
 export interface SidebarLinkProps {
   label: string
+  active: boolean
   headingType: 'h2' | 'h3'
 }
 
-export function SidebarLink({ label, headingType }: SidebarLinkProps) {
+export function SidebarLink({ label, active, headingType }: SidebarLinkProps) {
+  const activeFont = active ? 'font-bold' : ''
   const fontSize = headingType === 'h3' ? 'text-sm' : ''
   const margin = headingType === 'h3' ? 'ml-4' : ''
 
   const onClick = useCallback(() => {
     const element = document.getElementById(label)
-    if (element) window.scrollTo({ top: element.offsetTop - 15, behavior: 'smooth' })
+    if (element) window.scrollTo({ top: element.offsetTop - 20, behavior: 'smooth' })
   }, [label])
 
   return (
     <div
-      className={`cursor-pointer hover:underline text-teal-300 ${fontSize} ${margin}`}
+      className={`cursor-pointer hover:underline text-teal-300 ${activeFont} ${fontSize} ${margin}`}
       onClick={onClick}
     >
       {label}
