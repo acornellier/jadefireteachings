ea532c6e105e88c09209cc920d2d9cdf2b6e6dc6
Alex <alex@jintronix.com>
2024-12-07 10:13:33 -0500

    ui pack free

--- src/components/Guide/UserInterface/UiPack.tsx
+++ src/components/Guide/UserInterface/UiPack.tsx
@@ -7,13 +7,11 @@ export function UiPack() {
       <p>
         The best way to get my UI is through my{' '}
         <Link href="https://uipacks.wago.io/pack/ortemist-mistweaver-ui">Wago UI pack</Link>. This
-        includes all addon profiles and Weakauras in their most up-to-date configuration. It
-        requires a 1$ membership to <Link href="https://www.patreon.com/c/ortemis">my patreon</Link>
-        , or a subscription to my twitch.
+        includes all addon profiles and Weakauras in their most up-to-date configuration.
       </p>
       <p>
-        Alternatively, you can use the links below to get various parts of my UI for free! Please
-        note that these may not remain available forever.
+        Alternatively, you can use the links below to get various parts of my UI! Please note that
+        these may not remain available forever and might get out of date.
       </p>
     </SubSection>
   )
